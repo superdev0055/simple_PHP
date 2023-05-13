acd66951bc000cb79d78641e86fdea897848ff17
superdev0055 <superdev0055@gmail.com>
2023-05-13 09:59:50 +0100

    commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="assets/css/styles.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-	<title>CRUD application</title>
+	<title>Real-Estate</title>
 </head>
 <body>
 	<div class="container">
